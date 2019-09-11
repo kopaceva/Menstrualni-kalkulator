e640fc0b9bef643ebca3b6f365aa01d61162f5e0
kopaceva <49947133+kopaceva@users.noreply.github.com>
2019-09-11 12:38:27 +0200

    Dodani predlogi v vnosna okenca

--- kalkulator.py
+++ kalkulator.py
@@ -21,7 +21,12 @@ def r_izpis(razlika):
 @bottle.get('/')
 def prva_stran():
     c = model.citat()
-    return bottle.template('prva_stran.tpl', citat=c, danes_dat=danes_dat)
+    dan_vnos = danes.day
+    mesec_vnos = danes.month
+    leto_vnos = danes.year
+    dolzina_vnos = 28
+    trajanje_vnos = 5
+    return bottle.template('prva_stran.tpl', citat=c, danes_dat=danes_dat, dan_vnos=dan_vnos, mesec_vnos=mesec_vnos, leto_vnos=leto_vnos, dolzina_vnos=dolzina_vnos, trajanje_vnos=trajanje_vnos)
 
 @bottle.get('/cikel/')
 def datumi_na_dlani():
@@ -41,14 +46,14 @@ def datumi_na_dlani():
     p1 = d_izpis(danes + cikel_osebe.kdaj_prvi_plodni_dan())
     p2 = r_izpis(cikel_osebe.kdaj_prvi_plodni_dan())
     if dolzina_vnos < trajanje_vnos or (datum_v_py - razlika(7)) > danes or (danes - datum_v_py) > razlika(365):
-        return bottle.template('opozorilo.tpl', citat=c, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, danes_dat=danes_dat)
+        return bottle.template('opozorilo.tpl', citat=c, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, danes_dat=danes_dat, dan_vnos=dan_vnos, mesec_vnos=mesec_vnos, leto_vnos=leto_vnos)
     else:
         if (danes - datum_v_py) > razlika(dolzina_vnos):
             z1 = d_izpis(cikel_osebe.naslednji_cikel())
             z2 = r_izpis(abs(danes - cikel_osebe.naslednji_cikel()))
-            return bottle.template('dat2.tpl', citat=c, danes_dat=danes_dat, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, cikel_osebe=cikel_osebe, m1=m1, m2=m2, m3=m3, o1=o1, o2=o2, p1=p1, p2=p2, z1=z1, z2=z2)
+            return bottle.template('dat2.tpl', citat=c, danes_dat=danes_dat, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, cikel_osebe=cikel_osebe, m1=m1, m2=m2, m3=m3, o1=o1, o2=o2, p1=p1, p2=p2, z1=z1, z2=z2, dan_vnos=dan_vnos, mesec_vnos=mesec_vnos, leto_vnos=leto_vnos)
         else:
-            return bottle.template('dat1.tpl', citat=c, danes_dat=danes_dat, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, cikel_osebe=cikel_osebe, m1=m1, m2=m2, m3=m3, o1=o1, o2=o2, p1=p1, p2=p2)
+            return bottle.template('dat1.tpl', citat=c, danes_dat=danes_dat, datum_vnos=datum_vnos, trajanje_vnos=trajanje_vnos, dolzina_vnos=dolzina_vnos, cikel_osebe=cikel_osebe, m1=m1, m2=m2, m3=m3, o1=o1, o2=o2, p1=p1, p2=p2, dan_vnos=dan_vnos, mesec_vnos=mesec_vnos, leto_vnos=leto_vnos)
 
 
 bottle.run(reloader=True, debuger=True)
